3f216f8184e132e14081ea64cbc24ec1d9b39109
brantphoto <brant.barger@gmail.com>
2014-08-02 15:29:39 -0700

    finished tolist tests and about to start refectoring them

--- spec/features/todo_lists/create_spec.rb
+++ spec/features/todo_lists/create_spec.rb
@@ -49,5 +49,40 @@ describe "Creating todo list" do
     expect(page).to_not have_content("This is what I'm doing today.")
   end
 
+  it "displays an error when the todo list no description" do
+    expect(TodoList.count).to eq(0)
+
+    visit "/todo_lists"
+    click_link "New Todo list"
+    expect(page).to have_content("New todo_list")
+
+    fill_in "Title", with: "Grocery List"
+    fill_in "Description", with: ""
+    click_button "Create Todo list"
+
+    expect(page).to have_content("error")
+    expect(TodoList.count).to eq(0)
+
+    visit "/todo_lists"
+    expect(page).to_not have_content("This is what I'm doing today.")
+  end
+
+  it "displays an error when the description has less than 5 characters" do
+    expect(TodoList.count).to eq(0)
+
+    visit "/todo_lists"
+    click_link "New Todo list"
+    expect(page).to have_content("New todo_list")
+
+    fill_in "Title", with: "Grocery List"
+    fill_in "Description", with: "hi"
+    click_button "Create Todo list"
+
+    expect(page).to have_content("error")
+    expect(TodoList.count).to eq(0)
+
+    visit "/todo_lists"
+    expect(page).to_not have_content("This is what I'm doing today.")
+  end
 
 end
